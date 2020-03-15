7bd748ecba9677c580b4965f5f213d00410aa078
Aiden-Yoo <37614326+Aiden-Yoo@users.noreply.github.com>
2020-03-16 00:13:34 +0900

    200316_4-5.Meta_Class,Photos_Model

--- core/models.py
+++ core/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class TimeStampedModel(models.Model):
 
-    """ Tims Stamped Model """
+    """ Times Stamped Model """
 
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
