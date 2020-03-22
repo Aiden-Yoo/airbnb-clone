e5c40fa1a7f3d6dace94d99793ce0dc8fc30ef2b
Aiden-Yoo <37614326+Aiden-Yoo@users.noreply.github.com>
2020-03-22 20:15:47 +0900

    200322_7-3. Fnishing the Room Admin

--- rooms/admin.py
+++ rooms/admin.py
@@ -7,7 +7,13 @@ class ItemAdmin(admin.ModelAdmin):
 
     """ Item Admin Definition """
 
-    pass
+    list_display = (
+        "name",
+        "used_by",
+    )
+
+    def used_by(self, obj):
+        return obj.rooms.count()
 
 
 @admin.register(models.Room)
@@ -45,6 +51,7 @@ class RoomAdmin(admin.ModelAdmin):
         "check_out",
         "instant_book",
         "count_amenities",
+        "count_photos",
     )
 
     ordering = ("name", "price", "bedrooms")
@@ -71,7 +78,10 @@ class RoomAdmin(admin.ModelAdmin):
     def count_amenities(self, obj):
         return obj.amenities.count()
 
-    count_amenities.short_description = "count_amenities"
+    # count_amenities.short_description = "count_amenities"
+
+    def count_photos(self, obj):
+        return obj.photos.count()
 
 
 @admin.register(models.Photo)
