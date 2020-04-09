bd08664f17bf1d0e95fe46b2bc91ee96308a50e3
Aiden-Yoo <37614326+Aiden-Yoo@users.noreply.github.com>
2020-04-09 23:32:09 +0900

    200409_9-7. seed_lists

--- reviews/management/commands/seed_reviews.py
+++ reviews/management/commands/seed_reviews.py
@@ -15,7 +15,7 @@ class Command(BaseCommand):
             "--number",
             default=2,
             type=int,
-            help="How many users do you want me to create",
+            help="How many reviews do you want me to create",
         )
 
     def handle(self, *args, **options):
--- reviews/models.py
+++ reviews/models.py
@@ -33,6 +33,6 @@ class Review(core_models.TimeStampedModel):
             + self.check_in
             + self.value
         ) / 6
-        return round(avg, 2)
+        return round(avg, 1)
 
     rating_average.short_description = "AVG."
